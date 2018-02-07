cc63e1f52ded57c4dabfd33ad624d1781e7116e7
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-02-07 13:04:54 -0500

    Reset everything for 2018

--- files/script.js
+++ files/script.js
@@ -10,3 +10,35 @@ L.tileLayer("http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png", {
   subdomains: "abcd",
   maxZoom: 18
 }).addTo(map);
+
+var placesLayer = L.geoJSON(places, {
+  pointToLayer: function(feature, latlng) {
+    return L.circleMarker(latlng, getCircleMarker(feature));
+  },
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup(getPopupContent(feature));
+    layer.bindTooltip(L.tooltip({opacity: 0.7}).setContent(feature.properties.name));
+  }
+});
+map.addLayer(placesLayer);
+map.fitBounds(placesLayer.getBounds());
+
+function getCircleMarker(feature){
+  var color;
+  if (feature.properties.instances === 2) {
+    color = "#fc8d62";
+  } else {
+    color = "#8da0cb";
+  }
+  return { radius: 6, 
+    fillColor: color, 
+    color: "#333",
+    weight: 1,
+    opacity: 0.8,
+    fillOpacity: 0.8
+  };
+}
+
+function getPopupContent(feature){
+  return "<h4>" + feature.properties.name + "</h4>";
+}
